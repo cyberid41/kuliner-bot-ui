40e0201d3934b5187f1421d6c74b9c22cc1cbd8a
Edi Santoso <edicyber@gmail.com>
2017-07-21 17:33:36 +0700

    update carousel card

--- src/components/Cards/CarouselCard.js
+++ src/components/Cards/CarouselCard.js
@@ -1,45 +1,26 @@
 /* eslint-disable */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
 export default class CarouselCard extends Component {
-
+  static propTypes = {
+    result: PropTypes.object,
+  }
   render() {
+    const { result } = this.props;
+    console.log('list carousel =>', result);
     return (
       <div>
         <div className="clear"></div>
         <div className="category-slider">
           <div className="swiper-wrapper">
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index1.jpg" alt="img" />
-              <em>Ayam Bumbu Sedap</em>
-              <strong style={{ color: "#FFF" }}>IDR25000</strong>
-            </a>
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index2.jpg" alt="img" />
-              <em>Soto Daging</em>
-              <strong style={{ color: "#FFF" }}>IDR35000</strong>
-            </a>
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index3.jpg" alt="img" />
-              <em>Tahu Tek Sedap</em>
-              <strong style={{ color: "#FFF" }}>IDR15000</strong>
-            </a>
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index4.jpg" alt="img" />
-              <em>Bakso Beranak</em>
-              <strong style={{ color: "#FFF" }}>IDR35000</strong>
-            </a>
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index5.jpg" alt="img" />
-              <em>Rendang Sedap</em>
-              <strong style={{ color: "#FFF" }}>IDR45000</strong>
-            </a>
-            <a className="swiper-slide" href="#">
-              <img className="responsive-image" src="/images/pictures/index6.jpg" alt="img" />
-              <em>Pecel Istimewa</em>
-              <strong style={{ color: "#FFF" }}>IDR35000</strong>
-            </a>
+            {result.items && result.items.map((item, key) =>
+              <a className="swiper-slide" href="#">
+                <img className="responsive-image" src={item.image} alt="img" />
+                <em>{item.text}</em>
+                <strong style={{ color: "#FFF" }}>{item.price}</strong>
+              </a>
+            )}
           </div>
         </div>
       </div>
